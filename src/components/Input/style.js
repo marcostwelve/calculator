d605824bf97a880002375e34b6fa264170d3c735
marcostwelve <marcostwelve>
2024-02-07 20:35:15 -0300

    Calculadora React v1

---
+++ src/components/Input/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #AAAAFF;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 24px;
+  font-family: 'Roboto';
+
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    padding: 0 10px;
+    border: 0;
+    font-size: 24px;
+    font-family: 'Roboto';
+    color: #fff;
+  }
+`
